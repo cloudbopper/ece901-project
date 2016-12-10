c948dfd14ad0243d972d8b7c5915661ed6649ec5
honest16 <sneha16291@gmail.com>
2016-12-10 14:37:18 -0600

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from collections import deque
 import os
 import time
 import threading
-import urllib
+#FIXME
 #from urllib import urlretrieve
-from urllib.request import urlretrieve
+from urllib.request import urlretrieve 
 
 import lasagne
 import numpy as np
@@ -19,6 +19,29 @@ from sgd import sgd
 from thread_manager import TM
 
 # pylint: disable=superfluous-parens
+
+
+class WorkerThread2(threading.Thread):
+    def __init__(self, read_fn, train_fn, tid, inputs, targets):
+        # pylint: disable=too-many-arguments
+        threading.Thread.__init__(self)
+        self.tid = tid
+        self.train_fn = train_fn
+        self.read_fn = read_fn
+        self.inputs = inputs
+        self.targets = targets
+		self.retVal = False
+		
+	def run(self):	
+	    # Read and Train 
+        self.read_fn(self.tid)
+        out = self.train_fn(self.inputs, self.targets)
+        self.retVal = True      
+
+	def writeAvg(write_fn):
+		self.write_fn()
+        #self.write_fn(self.tid)		
+# Call the write function2 (no write/has to have the ret val/update at end, should have a run2 method for writing), 3creating thread class, 4test and validation
 
 class WorkerThread(threading.Thread):
     """Worker thread implementation"""
@@ -92,10 +115,213 @@ def main():
     parser.add_argument("-num_epochs", default=500, type=int)
 
     args = parser.parse_args()
-    pipeline(args)
+	#FIXME
+    argsReadIn = vars(args)
+    if argsReadIn["dropout_type"] == "overlapping":
+        pipeline(args)
+		#FIXME
+    elif argsReadIn["dropout_type"] == "disjoint":
+        pipelineDisjoint(args)
+		#FIXME
+	
+def pipelineDisjoint(args):
+    """Dropout experiment pipeline that implements disjoint dropout
+    """
+    # Load the dataset
+    print("Loading data...")
+    X_train, y_train, X_val, y_val, X_test, y_test = load_dataset()
+   
 
+    # Finally, launch the training loop.
+    print("Starting training...")
+    for epoch in range(args.num_epochs):
+	
+		# Create neural network model
+		print("At the start of every epoch, create subnetworks for each thread. Building the corresponding model and compiling functions...")
+		read_fn, write_avg_fn, train_fns, val_fn = gen_computational_graphs2(args)
+	
+	
+        # In each epoch, we do a full pass over the training data:
+        train_batches = 0
+        start_time = time.time()
+        for batch in iterate_minibatches2(X_train, y_train, 500, shuffle=True,args):
+            train_batches += args.threads
+			
+            inputsList, targetsList = batch
+			
+			threads = []
+			retVals = []
+			for thIn in range(args.threads):
+			    tid, train_fn = train_fns[thIn]
+				ti = WorkerThread2(read_fn,train_fn, thIn, inputsList[thIn], targetsList[thIn]) #FIXME1
+				# Call the write function2 (no write/has to have the ret val, should have a run2 method for writing), 3creating thread class, 4test and validation
+				threads.append(ti)
+				retVals.append(ti.retVal)
+				
+			for thIn in range(args.threads):
+				threads[thIn].start()
+            
+            while not all(retVals):
+                pass
+				
+            threads[-1].writeAvg(write_avg_fn)
+            
+		# FIXME: Check about this		
+        # And a full pass over the validation data:
+        val_err = 0
+        val_acc = 0
+        val_batches = 0
+        for batch in iterate_minibatches(X_val, y_val, args.batch_size, shuffle=False):
+            inputs, targets = batch
+            err, acc = val_fn(inputs, targets)
+            val_err += err
+            val_acc += acc
+            val_batches += 1
 
+        # Then we print the results for this epoch:
+        print("Epoch {} of {} took {:.3f}s".format(
+            epoch + 1, args.num_epochs, time.time() - start_time))
+        print("  training loss:\t\t{:.6f}".format(TM.train_err / train_batches))
+        print("  validation loss:\t\t{:.6f}".format(val_err / val_batches))
+        print("  validation accuracy:\t\t{:.2f} %".format(
+            val_acc / val_batches * 100))
 
+    # After training, we compute and print the test error:
+    test_err = 0
+    test_acc = 0
+    test_batches = 0
+    for batch in iterate_minibatches(X_test, y_test, 500, shuffle=False):
+        inputs, targets = batch
+        err, acc = val_fn(inputs, targets)
+        test_err += err
+        test_acc += acc
+        test_batches += 1
+    print("Final results:")
+    print("  test loss:\t\t\t{:.6f}".format(test_err / test_batches))
+    print("  test accuracy:\t\t{:.2f} %".format(
+        test_acc / test_batches * 100))
+
+	
+   		
+	
+
+	
+def build_custom_mlp(input_var, depth, width, drop_input,drop_hidden):
+    # By default, this creates the same network as `build_mlp`, but it can be
+    # customized with respect to the number and size of hidden layers. This
+    # mostly showcases how creating a network in Python code can be a lot more
+    # flexible than a configuration file. Note that to make the code easier,
+    # all the layers are just called `network` -- there is no need to give them
+    # different names if all we return is the last one we created anyway; we
+    # just used different names above for clarity.
+
+    # Input layer and dropout (with shortcut `dropout` for `DropoutLayer`):
+    network = lasagne.layers.InputLayer(shape=(None, 1, 28, 28),
+                                        input_var=input_var)
+    if drop_input:
+        network = lasagne.layers.dropout(network, p=drop_input)
+    # Hidden layers and dropout:
+    nonlin = lasagne.nonlinearities.rectify
+    for _ in range(depth):
+        network = lasagne.layers.DenseLayer(
+                network, width, nonlinearity=nonlin)
+        if drop_hidden:
+            network = lasagne.layers.dropout(network, p=drop_hidden)
+    # Output layer:
+    softmax = lasagne.nonlinearities.softmax
+    network = lasagne.layers.DenseLayer(network, 10, nonlinearity=softmax)
+    return network
+
+	
+	
+	
+#FIXME
+def gen_computational_graphs2(args):
+	"""Generates Theano functions for training/testing the network when the dropout is disjoint"""
+    # pylint: disable=too-many-locals
+    # Create separate training networks for each thread
+    train_fns = []#deque()
+    params_per_thread = []
+    for tid in range(args.threads):
+        # Prepare Theano variables for inputs and targets
+        input_var = T.tensor4('inputs_%d' % tid)
+        target_var = T.ivector('targets_%d' % tid)
+        
+        # Create neural network model (depending on first command line parameter)
+        print("Building model and compiling functions...")
+        depth = 2
+		width = 20
+		drop_in = None
+		drop_hid = 0.5
+        network = build_custom_mlp(input_var, int(depth), int(width), drop_in, float(drop_hid))
+   
+        # Create a loss expression for training, i.e., a scalar objective we want
+        # to minimize (for our multi-class problem, it is the cross-entropy loss):
+        prediction = lasagne.layers.get_output(network)
+        loss = lasagne.objectives.categorical_crossentropy(prediction, target_var)
+        loss = loss.mean()
+        # We could add some weight decay as well here, see lasagne.regularization.
+
+		
+        # Create update expressions for training, i.e., how to modify the
+        # parameters at each training step. Here, we'll use Stochastic Gradient
+        # Descent (SGD) with Nesterov momentum, but Lasagne offers plenty more.
+        params = lasagne.layers.get_all_params(network, trainable=True)
+        updates = lasagne.updates.nesterov_momentum(loss, params, learning_rate=0.01, momentum=0.9)
+		
+        # Compile a function performing a training step on a mini-batch (by giving
+        # the updates dictionary) and returning the corresponding training loss:
+        train_fn = theano.function([input_var, target_var], loss, updates=updates)
+        train_fns.append((tid, train_fn))
+
+		
+    # Compile a second function computing the validation loss and accuracy:
+    input_var = T.tensor4('input')
+    target_var = T.ivector('targets')
+    depth = 2
+    width = 20
+    drop_in = None
+    drop_hid = 0.5
+    network = build_custom_mlp(input_var, int(depth), int(width), drop_in, float(drop_hid))
+		
+		
+    # Create a loss expression for validation/testing. The crucial difference
+    # here is that we do a deterministic forward pass through the network,
+    # disabling dropout layers.
+    test_prediction = lasagne.layers.get_output(network, deterministic=True)
+    test_loss = lasagne.objectives.categorical_crossentropy(test_prediction,
+                                                            target_var)
+    test_loss = test_loss.mean()
+    # As a bonus, also create an expression for the classification accuracy:
+    test_acc = T.mean(T.eq(T.argmax(test_prediction, axis=1), target_var),
+                      dtype=theano.config.floatX)
+
+    val_fn = theano.function([input_var, target_var], [test_loss, test_acc])
+
+    # update params using per-thread params
+    gparams = lasagne.layers.get_all_params(network, trainable=True)
+    def read_fn(tid):
+        """Update per-thread params from global params"""
+        tparams = params_per_thread[tid]
+        for idx, _ in enumerate(gparams):
+            tparams[idx].set_value(gparams[idx].get_value())
+			
+    def write_avg_fn():
+        """Update global params from per-thread params"""
+        for idx, _ in enumerate(gparams):
+			indVal = 0
+		    for tid in range(args.threads):	
+			    tparams = params_per_thread[tid]
+				try:
+					indVal += tparams[idx].get_value()
+				except KeyError:
+					pass
+			indVal = (indVal*1.0)/args.threads	
+            gparams[idx].set_value(indVal)
+			
+    return read_fn, write_avg_fn, train_fns, val_fn
+
+	
 def pipeline(args):
     """Dropout experiment pipeline
     Note: currently only implements overlapping dropout
@@ -116,9 +342,12 @@ def pipeline(args):
         # In each epoch, we do a full pass over the training data:
         train_batches = 0
         start_time = time.time()
+        
         for batch in iterate_minibatches(X_train, y_train, 500, shuffle=True):
             train_batches += 1
             inputs, targets = batch
+			
+			
             TM.worker_cv.acquire()
             while TM.num_workers >= args.threads:
                 TM.worker_cv.wait()
@@ -269,7 +498,7 @@ def load_dataset():
     y_train = load_mnist_labels('train-labels-idx1-ubyte.gz')
     X_test = load_mnist_images('t10k-images-idx3-ubyte.gz')
     y_test = load_mnist_labels('t10k-labels-idx1-ubyte.gz')
-
+    
     # We reserve the last 10000 training examples for validation.
     X_train, X_val = X_train[:-10000], X_train[-10000:]
     y_train, y_val = y_train[:-10000], y_train[-10000:]
@@ -278,7 +507,43 @@ def load_dataset():
     # (It doesn't matter how we do this as long as we can read them again.)
     return X_train, y_train, X_val, y_val, X_test, y_test
 
-
+	
+def iterate_minibatches2(inputs, targets, batchsize, shuffle=False):
+    """Helper function to iterate over training data in mini-batches"""
+    assert len(inputs) == len(targets)
+	inputsList = []
+	targetsList = []
+	
+    if shuffle:
+        indices = np.arange(len(inputs))
+        np.random.shuffle(indices)
+		
+    numth = 0	
+    for start_idx in range(0, len(inputs) - batchsize + 1, batchsize):
+        if numth < args.threads:
+		    if shuffle:
+			    excerpt = indices[start_idx:start_idx + batchsize]
+			else:
+				excerpt = slice(start_idx, start_idx + batchsize)
+		    inputsList.append(inputs[excerpt])
+			targetsList.append(targets[excerpt])
+			numth += 1
+			
+		elif numth == args.threads:
+		    yield inputsList, targetsList
+		    inputsList =[]
+			targetsList =[]
+		    numth = 0
+			if shuffle:
+			    excerpt = indices[start_idx:start_idx + batchsize]
+			else:
+				excerpt = slice(start_idx, start_idx + batchsize)
+		    inputsList.append(inputs[excerpt])
+			targetsList.append(targets[excerpt])
+			numth += 1
+		
+	
+	
 def iterate_minibatches(inputs, targets, batchsize, shuffle=False):
     """Helper function to iterate over training data in mini-batches"""
     assert len(inputs) == len(targets)
