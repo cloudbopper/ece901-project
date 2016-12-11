3e418647bdc5bf4001ff224955c3754cd96313f6
honest16 <sneha16291@gmail.com>
2016-12-11 12:42:36 -0600

    Functionality for plots

---
+++ Plots.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+
+# Training Error versus the Number of Epoch for parallel and serial case
+def TrainErrVsEpoch(xlist,ylist):
+    assert len(xlist) == len(ylist)
+    for pIn in range(len(xlist)):
+        labelp = 'p = '+ str(pIn+1)		
+        plt.plot(xlist[pIn],ylist[pIn] ,label=labelp)
+    
+    #plt.axis([0, 6, 0, 20])
+    plt.ylabel('Training Loss')
+    plt.xlabel('Number of Epochs')	
+    plt.legend(loc='upper right')
+    plt.title('Training loss versus Number of epochs with Averaging across threads')
+    plt.show()    
+
+	
+#TODO: Another plot based on definition of speedup with respect serial case? (Speed up versus number of cores)	
+	
+	
+
+
